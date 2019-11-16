8505ae596dd0c60856c59719f31a3138691ea667
Steve Fox <steven.fox@gmail.com>
2019-11-16 17:16:59 +0000

    try a new component

---
+++ src/pages/contact.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import { HelmetDatoCms } from 'gatsby-source-datocms'
+import Img from 'gatsby-image'
+import Layout from "../components/layout"
+
+const Contact = ({ data: { contact } }) => (
+    <Layout>
+        <article className="sheet">
+            <div className="sheet__inner">
+                <h1 className="sheet__title">{contact.title}</h1>
+                <p className="sheet__lead">{contact.phoneNumber}</p>
+            </div>
+        </article>
+    </Layout>
+)
+
+export default Contact
+
+export const query = graphql`
+  query MyQuery {
+  contactPage {
+    intro(markdown: false)
+    phoneNumber
+    title
+  }
+}
+`
