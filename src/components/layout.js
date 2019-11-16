40feb8d5e20172ea2e5e4e6ce377f5de7da35312
Steve Fox <steven.fox@gmail.com>
2019-11-16 17:28:58 +0000

    that worked, now try to add the new navigation

--- src/components/layout.js
+++ src/components/layout.js
@@ -63,6 +63,9 @@ const TemplateWrapper = ({ children }) => (
             <li>
               <Link to="/about">About</Link>
             </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
           </ul>
           <p className="sidebar__social">
             {data.allDatoCmsSocialProfile.edges.map(({ node: profile }) => (
